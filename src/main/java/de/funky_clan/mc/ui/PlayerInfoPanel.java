41fd6df8d335bfddbbe325a9d8dab0afeae6d681
U-MATRIX-SEVEN\synopia <synopia@MATRIX-SEVEN.(none)>
2011-01-29 15:36:43 +0100

    added compas

--- src/main/java/de/funky_clan/mc/ui/PlayerInfoPanel.java
+++ src/main/java/de/funky_clan/mc/ui/PlayerInfoPanel.java
@@ -24,15 +24,45 @@ public class PlayerInfoPanel extends JPanel {
 
     public void updatePlayerPos( int x, int y, int z, int relX, int relY, int relZ, int angle ) {
         String text = String.format(
+                "Direction: %s\n"+
                 "Absolute World: %s\n"+
                 "Absolute Model: %s\n"+
                 "Relative to mid: %s\n",
+                formatDirection(angle),
                 formatCoord(x, y, z),
                 formatCoord(relX, relY, relZ),
                 formatCoord(relX-model.getWidth()/2, relY-model.getHeight()/2, relZ)
         );
 
         textArea.setText( text );
+    }
+
+    public String formatDirection( int angle ) {
+        angle %= 360;
+        while( angle<0 ) {
+            angle += 360;
+        }
+
+        String dir = "";
+        if( angle<45 ) {
+            dir = "W";
+        } else if( angle<2*45 ) {
+            dir = "NW";
+        } else if( angle<3*45 ) {
+            dir = "N";
+        } else if( angle<4*45 ) {
+            dir = "NE";
+        } else if( angle<5*45 ) {
+            dir = "E";
+        } else if( angle<6*45 ) {
+            dir = "SE";
+        } else if( angle<7*45 ) {
+            dir = "S";
+        } else if( angle<8*45 ) {
+            dir = "SW";
+        }
+
+        return dir;
     }
 
     public String formatCoord( int x, int y, int z ) {
