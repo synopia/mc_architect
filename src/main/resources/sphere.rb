2559b474e60fbb44b02f5aff18a4524dffe3f491
U-MATRIX-SEVEN\synopia <synopia@MATRIX-SEVEN.(none)>
2011-03-23 21:13:09 +0100

    sphere updated

--- src/main/resources/sphere.rb
+++ src/main/resources/sphere.rb
@@ -1,17 +1,15 @@
 java_import "de.funky_clan.mc.model.SliceType"
 
-mid_x = 607
+mid_x = 1217
 mid_y = 64
-mid_z = -14
-
-
+mid_z = -50
 
 def sphere g, r
   (-r..+r).each do |x|
     (-r..+r).each do |y|
       (-r..+r).each do |z|
         dist = Math.sqrt( x**2 + y**2 + z**2 )
-        if dist<r
+        if (r-dist).abs<1
           g.set_pixel x,y,z, 1
         end
       end
@@ -19,21 +17,5 @@ def sphere g, r
   end
 end
 
-
-def helix g, r, a, b, c, offset=0
-  (0..a*360).each do |deg|
-    rad = (deg) / 180.0 * Math::PI
-    z = c * rad
-
-    rad += offset/180.0 * Math::PI
-    x = r * Math.cos( b * rad )
-    y = r * Math.sin( b * rad )
-    g.set_pixel x, y, z, 1
-  end
-end
-
-#helix 7,3, 1, 1.5, 90
-#helix 7,3, 1, 1.5, 270
-
-@world.origin mid_x, mid_z, mid_y
-sphere @world, 30
+@world.set_origin mid_x, mid_y, mid_z
+sphere @world, 64
