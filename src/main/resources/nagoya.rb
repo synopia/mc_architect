7be5de5eb14c783b89a6f4ae1e2a533f44cec041
U-MATRIX-SEVEN\synopia <synopia@MATRIX-SEVEN.(none)>
2011-04-03 09:27:34 +0200

    0.3.5 - nagoya

--- src/main/resources/nagoya.rb
+++ src/main/resources/nagoya.rb
@@ -1,6 +1,8 @@
 java_import "de.funky_clan.mc.config.DataValues"
 
-@mid_x = -500
+# 10 bloecke in sueden
+
+@mid_x = -509
 @mid_y = 30
 @mid_z = 145
 
@@ -17,7 +19,7 @@ def run
 
     @slice_x.set_origin @mid_z,-@mid_y-62,@mid_x
 
-    @slice_x.set_scale 1, -1, -1
+    @slice_x.set_scale 1, -1, 1
 
     #@binvox.load @slice_x, "nagoya.binvox",  23,6,34, 127,127,98, DataValues::SAND.id
     @binvox.load @slice_x, "nagoya.binvox",  23,6,6, 127,127,127, DataValues::SAND.id
